5b0e3217a2c5f8e9a3ea8a454a1c653b20644f84
Nicolas <nicolohn@hotmail.com>
2018-10-31 23:15:55 -0300

    Modificación al TP

---
+++ src/app/Clases/usuario.ts
@@ -0,0 +1,19 @@
+import { Pelicula } from "./pelicula";
+
+export class Usuario {
+    id:number;
+    nombre:string;
+    apellido:string;
+    usuario:string;
+    contrasena:string;
+    telefono:string;
+    correo:string;
+    estado:string;
+    histPeliculas:string[]
+   
+
+        constructor(values:Object={})
+        {
+            Object.assign(this,values);
+        }
+}
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -9,23 +8,20 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-
-  it('should create the app', () => {
+  it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'VideoClub'`, () => {
+  }));
+  it(`should have as title 'TP-VideoClub'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('VideoClub');
-  });
-
-  it('should render title in a h1 tag', () => {
+    expect(app.title).toEqual('TP-VideoClub');
+  }));
+  it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to VideoClub!');
-  });
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to TP-VideoClub!');
+  }));
 });
